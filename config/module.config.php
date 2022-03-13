a78adffd85df662b55bb03b846ab87fa3553e842
Hossein Azizabadi Farahani <hossein@azizabadi.com>
2022-03-13 12:01:52 +0330

    Add module basic

---
+++ config/module.config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Notification;
+
+use Laminas\Mvc\Middleware\PipeSpec;
+use Laminas\Router\Http\Literal;
+
+return [
+    'service_manager' => [
+        'aliases'   => [
+
+        ],
+        'factories' => [
+
+        ],
+    ],
+
+    'router' => [],
+
+    'view_manager' => [
+        'strategies' => [
+            'ViewJsonStrategy',
+        ],
+    ],
+];
