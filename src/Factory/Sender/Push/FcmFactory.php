ec159eae848901b92e74e2f226d07a737d937d53
kerloper <karimijavad990@gmail.com>
2024-05-13 04:29:10 +0330

    set config and limitation for push

--- src/Factory/Sender/Push/FcmFactory.php
+++ src/Factory/Sender/Push/FcmFactory.php
@@ -17,6 +17,9 @@ class FcmFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Fcm
     {
-        return new Fcm();
+        $config = $container->get('config');
+        return new Fcm(
+            $config['notification']
+        );
     }
 }
--- src/Sender/Push/Fcm.php
+++ src/Sender/Push/Fcm.php
@@ -5,46 +5,58 @@ namespace Notification\Sender\Push;
 
 class Fcm implements PushInterface
 {
+
+    protected array $config;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+
     public function send($config, $params): void
     {
-        $config    = $config["fcm"];
-        $url       = $config["url"];
-        $token     = $params['device_token'];
+        $config = $config["fcm"];
+        $url = $config["url"];
+        $token = $params['device_token'];
         $serverKey = $config["server_key"];
 
-        $title        = $params['title'];
-        $in_app_title = $params['in_app_title'];
-        $body         = $params["body"];
-        $in_app_body  = $params["body"];
-        $type         = $params["type"];
-        $in_app       = $params["in_app"];
-        $image_url    = $params["image_url"];
-        $event        = $params["event"];
+        $title = $this->utility($params['title']);
+        $in_app_title = $this->utility($params['in_app_title']);
+        $body = $this->utility($params["body"]);
+        $in_app_body = $this->utility($params["body"]);
+        $type = $params["type"];
+        $in_app = $params["in_app"];
+        $image_url = $params["image_url"];
+        $event = $params["event"];
 
-        $aps         = ['mutable-content' => 1];
-        $payload     = ['aps' => $aps];
+        $aps = ['mutable-content' => 1];
+        $payload = ['aps' => $aps];
         $fcm_options = ['image' => $image_url];
-        $apns        = ['payload' => $payload, 'fcm_options' => $fcm_options];
+        $apns = ['payload' => $payload, 'fcm_options' => $fcm_options];
 
         $notification = [
-            'title'             => $title,
+            'title' => $title,
             "content_available" => true,
-            "mutable_content"   => true,
-            'body'              => $body,
-            'image'             => $image_url,
-            'sound'             => 'default',
-            'badge'             => '1',
+            "mutable_content" => true,
+            'body' => $body,
+            'image' => $image_url,
+            'sound' => 'default',
+            'badge' => '1',
         ];
-        $notData      = [
+        $notData = [
             'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
-            'type'         => $type,
-            'in_app_body'  => $in_app_body,
-            'in_app'       => $in_app,
+            'type' => $type,
+            'in_app_body' => $in_app_body,
+            'in_app' => $in_app,
             'in_app_title' => $in_app_title,
-            'event'        => $event,
-            "image_url"    => $image_url,
+            'event' => $event,
+            "image_url" => $image_url,
         ];
-        $has_data     = true;
+        $has_data = true;
         if (isset($params['custom_information'])) {
             $notData['custom_information'] = $params['custom_information'];
             if (isset($params['custom_information']['is_only_data'])) {
@@ -52,26 +64,34 @@ class Fcm implements PushInterface
             }
 
             if (isset($params['custom_information']['notification_information'])) {
-                $notification['title']   = $params['custom_information']['notification_information']['title'];
-                $notification['body']    = $params['custom_information']['notification_information']['body'];
+                $notification['title'] = $params['custom_information']['notification_information']['title'];
+                $notification['body'] = $params['custom_information']['notification_information']['body'];
                 $notData['in_app_title'] = $params['custom_information']['notification_information']['in_app_title'];
-                $notData['in_app_body']  = $params['custom_information']['notification_information']['in_app_body'];
+                $notData['in_app_body'] = $params['custom_information']['notification_information']['in_app_body'];
             }
         }
         $arrayToSend = ['to' => $token, 'data' => $notData, 'apns' => $apns, 'priority' => 'high'];
         if ($has_data) {
             $arrayToSend['notification'] = $notification;
         }
-        $json      = json_encode($arrayToSend);
-        $headers   = [];
+        $json = json_encode($arrayToSend);
+        $headers = [];
         $headers[] = 'Content-Type: application/json';
         $headers[] = 'Authorization: key=' . $serverKey;
-        $ch        = curl_init();
+        $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
+    }
+
+    private function utility($text): string
+    {
+        $limitation = $this->config['push']['setting']['limitation'];
+        $xss = $this->config['push']['setting']['xss'];
+        $text = $limitation['status'] ? ((strlen($text) > $limitation['length']) ? substr($text, 0, $limitation['length']) . '...' : $text) : $text;
+        return $xss['status'] ? htmlspecialchars($text, ENT_QUOTES, 'UTF-8') : $text;
     }
 }
