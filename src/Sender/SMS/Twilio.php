8530549eb34f7d48a2ec48064bc582b9f78b8c88
Hossein Azizabadi Farahani <hossein.azizabadi@gmail.com>
2024-12-22 14:38:26 +0330

    Work on notification

--- src/Sender/SMS/Twilio.php
+++ src/Sender/SMS/Twilio.php
@@ -2,9 +2,25 @@
 
 namespace Pi\Notification\Sender\SMS;
 
+use Twilio\Exceptions\ConfigurationException;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Client;
+
 class Twilio implements SMSInterface
 {
+    /**
+     * @throws ConfigurationException
+     * @throws TwilioException
+     */
     public function send($config, $params): void
     {
+        $client = new Client($config['twilio']['sid'], $config['twilio']['token']);
+        $client->messages->create(
+            $params['to'],
+            [
+                'from' => $config['twilio']['number'],
+                'body' => sprintf('Your OTP code is: %s', $params['code']),
+            ]
+        );
     }
 }
